d911cf84e62f6b8997ee3e3fda14b60d2c08f296
Bob Jacobs <bob.jacobs@outlook.com>
2020-04-05 15:46:32 -0400

    adding routes

--- TodoApi/frontend/src/App.tsx
+++ TodoApi/frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
@@ -9,16 +13,21 @@ import { fontFamily, fontSize, gray2 } from './Styles';
 function App() {
   const unused = 'something';
   return (
-    <div 
-       css={css` 
-         font-family: ${fontFamily}; 
-         font-size: ${fontSize}; 
-         color: ${gray2}; 
-        `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div 
+        css={css` 
+          font-family: ${fontFamily}; 
+          font-size: ${fontSize}; 
+          color: ${gray2}; 
+          `}
+      >
+        <Header />
+        <Route path="/" component={HomePage} /> 
+        <Route path="/search" component={SearchPage} />
+        <Route path="/ask" component={AskPage} />
+        <Route path="/signin" component={SignInPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
