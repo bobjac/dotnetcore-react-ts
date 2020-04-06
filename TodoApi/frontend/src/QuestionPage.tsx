e221b2a5c3d2f73eb1a0bf322cb826b97ec7fdb8
Bob Jacobs <bob.jacobs@outlook.com>
2020-04-06 10:14:12 -0400

    adding updated to the QuestionPage to display question based on questionId

--- TodoApi/frontend/src/QuestionPage.tsx
+++ TodoApi/frontend/src/QuestionPage.tsx
@@ -1,6 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useState, Fragment, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Page } from './Page';
+import { QuestionData, getQuestion } from './QuestionsData';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { gray3, gray6 } from './Styles';
+
 
 interface RouteParams {
     questionId: string;
@@ -8,5 +13,63 @@ interface RouteParams {
 
 export const QuestionPage: FC<RouteComponentProps<RouteParams>> = ({
     match
-}) => <Page>Question Page { match.params.questionId }</Page>;
+}) => {
+    const [question, setQuestion] = useState<QuestionData | null>(null);
+    useEffect(() => {
+        const doGetQuestion = async (questionId: number) => {
+          const foundQuestion = await getQuestion(questionId);
+          setQuestion(foundQuestion);
+        };
+        if (match.params.questionId) {
+          const questionId = Number(match.params.questionId);
+          doGetQuestion(questionId);
+        }
+    }, [match.params.questionId]);  
+    return (
+        <Page>
+            <div
+                css={css`
+                background-color: white;
+                padding: 15px 20px 20px 20px;
+                border-radius: 4px;
+                border: 1px solid ${gray6};
+                box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+                `}
+            >
+                <div
+                css={css`
+                    font-size: 19px;
+                    font-weight: bold;
+                    margin: 10px 0px 5px;
+                `}
+                >
+                {question === null ? '' : question.title}
+                </div>
+                {question !== null && (
+                    <Fragment>
+                        <p
+                        css={css`
+                            margin-top: 0px;
+                            background-color: white;
+                        `}
+                        >
+                        {question.content}
+                        </p>
+                        <div
+                            css={css`
+                                font-size: 12px;
+                                font-style: italic;
+                                color: ${gray3};
+                            `}
+                            >
+                            {`Asked by ${question.userName} on
+                                ${question.created.toLocaleDateString()} 
+                                ${question.created.toLocaleTimeString()}`}
+                            </div>
+                    </Fragment>
+                    )}
+            </div>
+        </Page>
+    );
+};
 
