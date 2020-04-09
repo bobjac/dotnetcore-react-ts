5828766375550897097988ae6b12be7e26d35388
Bob Jacobs <bob.jacobs@outlook.com>
2020-04-09 10:15:12 -0400

    updating route handling

--- TodoApi/frontend/src/App.tsx
+++ TodoApi/frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
-import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { NotFoundPage } from './NotFoundPage';
@@ -11,6 +10,8 @@ import { QuestionPage } from './QuestionPage';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
+
+const AskPage = lazy(() => import('./AskPage'));
 
 function App() {
   const unused = 'something';
@@ -28,7 +29,22 @@ function App() {
           <Redirect from="/home" to="/" />
           <Route exact path="/" component={HomePage} /> 
           <Route path="/search" component={SearchPage} />
-          <Route path="/ask" component={AskPage} />
+          <Route path="/ask">
+            <Suspense
+              fallback={
+                <div
+                  css={css`
+                    margin-top: 100px;
+                    text-align: center;
+                  `}
+                >
+                  Loading....
+                </div>
+              }
+            >
+              <AskPage />
+            </Suspense>
+          </Route>
           <Route path="/signin" component={SignInPage} />
           <Route path="/questions/:questionId" component={QuestionPage} />
           <Route component={NotFoundPage} />
