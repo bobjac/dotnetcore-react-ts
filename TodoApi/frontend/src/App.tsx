a9ca267fd6a2b9620fb03c52d561b9dbb07a8560
Bob Jacobs <bob.jacobs@outlook.com>
2020-04-20 23:31:37 -0400

    adding redux store to the App component

--- TodoApi/frontend/src/App.tsx
+++ TodoApi/frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import React, { lazy, Suspense } from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from './Store';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import { HeaderWithRouter as Header } from './Header';
@@ -13,44 +15,48 @@ import { fontFamily, fontSize, gray2 } from './Styles';
 
 const AskPage = lazy(() => import('./AskPage'));
 
+const store = configureStore();
+
 function App() {
   const unused = 'something';
   return (
-    <BrowserRouter>
-      <div 
-        css={css` 
-          font-family: ${fontFamily}; 
-          font-size: ${fontSize}; 
-          color: ${gray2}; 
-          `}
-      >
-        <Header />
-        <Switch>
-          <Redirect from="/home" to="/" />
-          <Route exact path="/" component={HomePage} /> 
-          <Route path="/search" component={SearchPage} />
-          <Route path="/ask">
-            <Suspense
-              fallback={
-                <div
-                  css={css`
-                    margin-top: 100px;
-                    text-align: center;
-                  `}
-                >
-                  Loading....
-                </div>
-              }
-            >
-              <AskPage />
-            </Suspense>
-          </Route>
-          <Route path="/signin" component={SignInPage} />
-          <Route path="/questions/:questionId" component={QuestionPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div 
+          css={css` 
+            font-family: ${fontFamily}; 
+            font-size: ${fontSize}; 
+            color: ${gray2}; 
+            `}
+        >
+          <Header />
+          <Switch>
+            <Redirect from="/home" to="/" />
+            <Route exact path="/" component={HomePage} /> 
+            <Route path="/search" component={SearchPage} />
+            <Route path="/ask">
+              <Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading....
+                  </div>
+                }
+              >
+                <AskPage />
+              </Suspense>
+            </Route>
+            <Route path="/signin" component={SignInPage} />
+            <Route path="/questions/:questionId" component={QuestionPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
