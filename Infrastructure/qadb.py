c4bc1d87812697f55219c9bac887a4deef632f20
Bob Jacobs <bob.jacobs@outlook.com>
2020-04-25 14:18:35 -0400

    adding base infrastrucutre python and sql scripts

---
+++ Infrastructure/qadb.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+def create_sql_container(sql_container, sa_password):
+    f = os.popen('date')
+    now = f.read()
+    print("Today is ", now)
+
+    sa_password_param = "\"SA_Password=" + sa_password + "\""
+    print("sa_password: " + sa_password_param)
+
+    print("sql_container: " + sql_container)
+
+    subprocess.call(["docker", "run", "-e", "\"ACCEPT_EULA\"", "-e", sa_password_param, "-p", "1444:1433", "--name", "sql2", "-d", sql_container])
---
+++ Infrastructure/setup.py
@@ -0,0 +1,18 @@
+import sys
+import qadb
+
+def main(container_name, sa_password):
+    container = qadb.create_sql_container(container_name, sa_password)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("This script will create the containers needed to run this sample locally.") 
+        print("Usage:   python3 setup.py <Container> <SA Password>")
+    else:
+        container_name = sys.argv[1]
+        print("The first argument is ", container_name)
+
+        sa_password = sys.argv[2]
+        print("The second argument is ", sa_password)
+
+        main(container_name, sa_password)
