15d2e1328e5333ad116bd248241c06bf85c211cd
Bob Jacobs <bob.jacobs@outlook.com>
2020-04-27 01:02:33 -0400

    adding write operations to the repository

---
+++ TodoApi/Data/Models/QuestionPutRequest.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace QandA.Data.Models
+{
+    public class QuestionPutRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
